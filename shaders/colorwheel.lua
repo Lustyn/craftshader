6f41ad5aa7e0e3df8ce893da1e45b9b0b03e9c35
Justyn <egot@istic.pw>
2020-03-12 17:28:30 -0500

    Fix color wheel shader and add images

--- shaders/colorwheel.lua
+++ shaders/colorwheel.lua
@@ -1,12 +1,13 @@
 local vec2 = require("math.vec2")
 local vec3 = require("math.vec3")
 local color = require("math.color")
+local umath = require("math.umath")
 
 return function(pos, c, buf)
     local npos = (pos - 1) / (buf.size - 1)
     local to_center = vec2(0.5, 0.5) - npos
     local angle = math.atan2(to_center.y, to_center.x)
-    local radius = to_center:length() * 2
-
-    return color.hsv2rgb(vec3(angle / (2 * math.pi) + 1, radius, 1))
+    local radius = umath.clamp(to_center:length() * 2, 0, 1)
+    
+    return color.hsv2rgb(vec3(angle / (2 * math.pi) + 0.5, radius, 1))
 end
--- shaders/hsb.lua
+++ shaders/hsb.lua
@@ -1,6 +1,7 @@
 local color = require("math.color")
+local vec3 = require("math.vec3")
 
-return function(pos, color, buf)
+return function(pos, c, buf)
     local npos = (pos - 1) / (buf.size - 1)
-    return color.hsv2rgb(npos.x, npos.y, 1)
+    return color.hsv2rgb(vec3(npos.x, npos.y, 1))
 end
